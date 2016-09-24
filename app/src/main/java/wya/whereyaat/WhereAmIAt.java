7211161f043abf494d42bd3e14228a8490a29511
Aditya <aditya.nadkarni12@gmail.com>
2016-09-24 15:41:23 -0400

    Some of the changes

---
+++ app/src/main/java/wya/whereyaat/WhereAmIAt.java
@@ -0,0 +1,61 @@
+package wya.whereyaat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class WhereAmIAt extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback,GoogleApiClient.OnConnectionFailedListener,View.OnTouchListener {
+
+    private int protectionGame = 0;
+    private Location location;
+    private GoogleApiClient myGoogleApiClient = null;
+    private Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_where_am_iat);
+        b = (Button)findViewById(R.id.showLocationButton);
+        b.setOnTouchListener(this);
+        if(myGoogleApiClient == null){
+            myGoogleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(LocationServices.API).build();
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},protectionGame);
+        }
+        else{
+            location = LocationServices.FusedLocationApi.getLastLocation(myGoogleApiClient);
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        System.out.print(connectionResult.describeContents());
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_BUTTON_PRESS){
+            try {
+                location = LocationServices.FusedLocationApi.getLastLocation(myGoogleApiClient);
+            }
+            catch (SecurityException e){
+                e.printStackTrace();
+            }
+            b.setText("Latitude"+ location.getLatitude());
+        }
+        return true;
+    }
+}
