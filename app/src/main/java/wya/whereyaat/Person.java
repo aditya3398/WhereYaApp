a29d9aec324eaadba78efd1035af57a1dbb9533b
bshah <bshah2016@gmail.com>
2016-09-25 08:25:49 -0400

    updacdsdf

--- app/src/main/java/wya/whereyaat/Person.java
+++ app/src/main/java/wya/whereyaat/Person.java
@@ -8,10 +8,14 @@ public class Person {
 
     private String name = "";
     private String location = "";
+    private double latitude;
+    private double longitude;
 
-    public Person(String name, String location){
+    public Person(String name, String location, double longitudeHolder, double latHolder){
         this.name = name;
         this.location = location;
+        latitude = latHolder;
+        longitude = longitudeHolder;
     }
 
     public String getName(){
@@ -22,7 +26,27 @@ public class Person {
         return location;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
+    public double getLatitude() {
+        return latitude;
+    }
 
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
